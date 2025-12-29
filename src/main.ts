65c3ae04f9d19d25216ebbe9757796eec83363fc
Daniel Marques Barbosa <danielmarquesbarbosa.seget@gmail.com>
2025-12-29 14:05:46 -0300

    Ajusta CORS e cria Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
-  // Adiciona headers para evitar cache
+  app.enableCors({
+    origin: [
+      'http://agendamento.127.0.0.1.nip.io',
+      'http://localhost:8081',
+      'http://127.0.0.1:8081',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.use((req, res, next) => {
     res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.setHeader('Pragma', 'no-cache');
@@ -18,12 +26,13 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true, // Habilita transformação automática
+      transform: true,
       transformOptions: {
-        enableImplicitConversion: true, // Converte tipos automaticamente
+        enableImplicitConversion: true,
       },
     }),
   );
+  
   app.setGlobalPrefix('api');
   await app.listen(3001);
 }
